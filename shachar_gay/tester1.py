b33659c790c23b8b782bcd29c41bf840489f436e
shachar yust <yustshachar@gmail.com>
2021-06-21 14:53:56 +0300

    check

--- shachar_gay/tester1.py
+++ shachar_gay/tester1.py
@@ -13,6 +13,8 @@ driver.find_element_by_id("tabletsImg").click()
 # כניסה לעמוד מוצר
 driver.find_element_by_id("17").click()
 
+#שינוי לבדיקה בלה בלה בלה
+
 # בחירת צבע שחור
 driver.find_element_by_css_selector(".BLACK").click()
 
